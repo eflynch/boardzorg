8b3aa6f7a77dc013c0cefb85251656eb52e7dad3
Russell McClellan <russell.mcclellan@gmail.com>
2020-04-05 01:59:35 -0400

    generalize

--- duneserver/socketapi.py
+++ duneserver/socketapi.py
@@ -12,49 +12,53 @@ class _SubscriptionData:
     clients: any
     callback: any
 
-roles_subs = {}
 
+@dataclass
+class _NamespaceInfo:
+    name: str
+    serialize: any
+    room_for_data: any
 
-def _serialize_roles(roles):
-    return {
-        "assigned_roles": list(roles.values())
-    }
+def connect_namespace(socketio, namespace_info):
+    subs = {}
 
+    def _connect(session_id, client_id, data):
+        if session_id in subs:
+            subs[session_id].clients.append(client_id)
+        else:
+            def _on_change(session, roles):
+                emit(namespace_info.name,
+                     namespace_info.serialize(session, roles, data),
+                     room=room_for_data(data),
+                     namespace=f"/{namespace_info.name}")
+                subs[session_id] = _SubscriptionData(
+                    clients=[client_id],
+                    callback=_on_change)
+                subscribe(session_id, subs[session_id].callback)
 
-def _connect_roles(session_id, client_id):
-    if session_id in roles_subs:
-        roles_subs[session_id].clients.append(client_id)
-    else:
-        def _on_roles(_session, roles):
-            emit('roles',
-                 _serialize_roles(roles),
-                 room=f"roles/{session_id}",
-                 namespace="/roles")
-        roles_subs[session_id] = _SubscriptionData(
-            clients=[client_id],
-            callback=_on_roles)
-        subscribe(session_id, roles_subs[session_id].callback)
+    def _disconnect(client_id):
+        for session_id in subs:
+            subs[session_id].clients.remove(client_id)
+            if len(subs[session_id].clients) == 0:
+                unsubscribe(session_id, subs[session_id].callback)
+                del subs[session_id]
+                break
 
+    @socketio.on('join', namespace=f"/{namespace_info.name}")
+    def handle_join(data):
+        session_id = data["session_id"]
+        _connect(session_id, request.sid, data)
+        with SessionWrapper(session_id) as (session, roles):
+            emit('roles', namespace_info.serialize(session, roles, data))
+            join_room(namespace_info.room_for_data(data))
 
-def _disconnect_roles(client_id):
-    for session_id in roles_subs:
-        roles_subs[session_id].clients.remove(client_id)
-        if len(roles_subs[session_id].clients) == 0:
-            unsubscribe(session_id, roles_subs[session_id].callback)
-            del roles_subs[session_id]
-            break
-
+    @socketio.on('disconnect', namespace=f"/{namespace_info.name}")
+    def handle_disconnect_roles():
+        _disconnect(request.sid)
 
 def connect_socketio(socketio):
-    @socketio.on('join', namespace="/roles")
-    def handle_join_roles(data):
-        session_id = data["session_id"]
-
-        _connect_roles(session_id, request.sid)
-        with SessionWrapper(session_id) as (_session, roles):
-            emit('roles', _serialize_roles(roles))
-            join_room(f"roles/{session_id}")
-
-    @socketio.on('disconnect', namespace="/roles")
-    def handle_disconnect_roles():
-        _disconnect_roles(request.sid)
+    connect_namespace(socketio, _NamespaceInfo(
+        name="roles",
+        serialize=lambda _session, roles, _data: { "assigned_roles": list(roles.values()) },
+        room_for_data=lambda data: f"{data['session_id']}/roles",
+    ))
