f931bb324dae2564ea60a61b50d7400636443b73
Evan Lynch <elynch@izotope.com>
2020-04-05 18:16:35 -0400

    Allow looking at cards and looking into discard pile

--- duneclient/src/components/card.js
+++ duneclient/src/components/card.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {randInt} from '../utils';
 
 
-export default function Card({type, name, selected, onClick, width}) {
+export default function Card({type, name, selected, onClick, width, children, peak}) {
+
+    const [examine, setExamine] = useState(false);
+
     if (width === undefined) {
         width = 125;
     }
     if (name === "Cheap-Hero/Heroine"){
         name = ["Cheap-Hero", "Cheap-Heroine"][randInt(0,1)];
     }
-    return <img
-        src={`/static/app/png/${type}-${name.replace(" ", "-")}.png`}
-        className={"card" + (selected ? " selected" : "") + (onClick ? " active" : "")}
-        onClick={onClick}
-        width={width} />;
+    let examineStyle = {
+        zIndex: 1000,
+        position: "fixed",
+        top: 0,
+        left: 0
+    };
+
+    return (
+        <div style={examine ? examineStyle : {position:"relative"}} >
+            <img
+                src={`/static/app/png/${type}-${name.replace(" ", "-")}.png`}
+                className={"card" + (selected ? " selected" : "") + (onClick ? " active" : "") + (peak ? " peak": "")}
+                onClick={(e)=>{
+                    if (examine) {
+                        setExamine(false);
+                        return;
+                    }
+                    if (e.shiftKey){
+                        setExamine(true);
+                        return;
+                    }
+                    if (onClick !== undefined) {
+                        onClick(e);
+                    }
+                }}
+                width={examine ? 400 : width} />
+            {children}
+        </div>
+        );
 };
--- duneclient/src/components/deck.js
+++ duneclient/src/components/deck.js
@@ -5,14 +5,38 @@ import Card from './card';
 
 export default function Deck({facedown, faceup, type}) {
     const Faceup = (faceup) => {
-        if (faceup.length) {
-            return <Card type={type} name={faceup[0]} width={100}/>;
+        return faceup.reverse().map((name, i )=> {
+            return (
+                <div key={name} style={i != 0 ? {position: "absolute", top:0, left: 0} : undefined}>
+                    <Card type={type} name={name} width={100}/>
+                </div>
+            );
+        });
+    };
+
+    const Facedown = (facedown) => {
+        let next = <div/>;
+        if (facedown.next) {
+            next = <Card type={type} name={facedown.next} width={100} peak={true} />;
         }
+        if (facedown.length) {
+            return (
+                <Card type={type} name="Reverse" width={100}>
+                    <div style={{position: "absolute", color:"white", top:4, right: 4, fontSize: 10}}>{facedown.length} Remain</div>
+                    <div style={{position: "absolute", top:0, left: 0}}>
+                        {next}
+                    </div>
+                </Card>
+            );
+        }
+        return <Card type={type} name="Reverse" width={100}/>
     };
     return (
         <div style={{display:"flex",flexDirection: "column"}}>
-            <Card type={type} name="Reverse" width={100}/>
-            {Faceup(faceup)}
+            {Facedown(facedown)}
+            <div style={{position: "relative"}}>
+                {Faceup(faceup)}
+            </div>
         </div>
     );
 };
