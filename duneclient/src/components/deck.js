a5897a9776e1767e858b1094e292e204419aa4e3
Evan Lynch <elynch@izotope.com>
2020-04-05 13:41:49 -0400

    Allow empty discard pile

--- duneclient/src/components/deck.js
+++ duneclient/src/components/deck.js
@@ -4,10 +4,15 @@ import Card from './card';
 
 
 export default function Deck({facedown, faceup, type}) {
+    const Faceup = (faceup) => {
+        if (faceup.length) {
+            return <Card type={type} name={faceup[0]} width={100}/>;
+        }
+    };
     return (
         <div style={{display:"flex",flexDirection: "column"}}>
             <Card type={type} name="Reverse" width={100}/>
-            <Card type={type} name={faceup[0]} width={100} />
+            {Faceup(faceup)}
         </div>
     );
 };
