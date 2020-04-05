a7f341b99abb1b87e75499567869809ceea91ed8
Evan Lynch <elynch@izotope.com>
2020-04-04 22:36:34 -0400

    Factor out faction-order

---
+++ duneclient/src/components/faction-order.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Logo = ({faction, diameter, x, y, ...props}) => {
+    return <image {...props} xlinkHref={`/static/app/png/${faction}_logo.png`} x={x} y={y} width={diameter} height={diameter}/>;
+};
+
+export default function FactionOrder({factions}) {
+    return (
+        <svg width="600px" height="100px" viewBox="2.5 0 1 1">
+            {factions.map((factionInfo, i)=> {
+                let opacity = factionInfo.label === "pass" ? 0.2 : 1.0;
+                let circle = <g/>
+                if (factionInfo.active) {
+                    circle = <circle cx={i+0.5} cy={0.5} r={0.48} style={{
+                        fillOpacity: 0.,
+                        strokeWidth: 0.04,
+                        stroke:"white"
+                    }} />;
+                }
+                return (
+                    <g key={i}>
+                        <Logo x={i} y={0} faction={factionInfo.faction} diameter={1} style={{
+                            opacity: opacity
+                        }} />
+                        {circle}
+                        <text x={i+.5} y={0.6} textAnchor="middle" style={{
+                            fill: "white",
+                            stroke: "black",
+                            strokeWidth: 0.02,
+                            font: `bold 0.35px sans-serif`}}>{factionInfo.label}</text>
+                    </g>
+                );
+            })}
+        </svg>
+    );
+}
--- duneclient/src/rounds/bidding.js
+++ duneclient/src/rounds/bidding.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import TreacheryCard from '../components/treachery-card';
+import FactionOrder from '../components/faction-order';
 
 const getFactionOrder = (roundstate, logoPositions, stormSector) => {
     const factionOrder = []
@@ -15,40 +16,6 @@ const getFactionOrder = (roundstate, logoPositions, stormSector) => {
     return factionOrder;
 };
 
-const Logo = ({faction, diameter, x, y, ...props}) => {
-    return <image {...props} xlinkHref={`/static/app/png/${faction}_logo.png`} x={x} y={y} width={diameter} height={diameter}/>;
-};
-
-const FactionOrder = ({factions}) => {
-    return (
-        <svg width="600px" height="100px" viewBox="2.5 0 1 1">
-            {factions.map((factionInfo, i)=> {
-                let opacity = factionInfo.label === "pass" ? 0.2 : 1.0;
-                let circle = <g/>
-                if (factionInfo.active) {
-                    circle = <circle cx={i+0.5} cy={0.5} r={0.48} style={{
-                        fillOpacity: 0.,
-                        strokeWidth: 0.04,
-                        stroke:"white"
-                    }} />;
-                }
-                return (
-                    <g key={i}>
-                        <Logo x={i} y={0} faction={factionInfo.faction} diameter={1} style={{
-                            opacity: opacity
-                        }} />
-                        {circle}
-                        <text x={i+.5} y={0.6} textAnchor="middle" style={{
-                            fill: "white",
-                            stroke: "black",
-                            strokeWidth: 0.02,
-                            font: `bold 0.35px sans-serif`}}>{factionInfo.label}</text>
-                    </g>
-                );
-            })}
-        </svg>
-    );
-}
 
 class Bidding extends React.Component {
     getCards () {
@@ -63,15 +30,6 @@ class Bidding extends React.Component {
             cards.push(<TreacheryCard key={i} name="Reverse"/>);
         }
         return cards;
-    }
-    getBids () {
-        return Object.keys(this.props.roundstate.stage_state.bids).map((faction) => {
-            return (
-                <li key={faction}>
-                    {faction} : {this.props.roundstate.stage_state.bids[faction]}
-                </li>
-            );
-        });
     }
     render () {
         let {roundstate, logoPositions, stormSector} = this.props;
