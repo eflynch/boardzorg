0c2916da7220b5ffc33c6ba1d13e446029a10b88
Evan Lynch <evan.f.lynch@gmail.com>
2022-05-20 10:57:09 -0400

    Fix rename

--- apply_skin.py
+++ apply_skin.py
@@ -33,8 +33,8 @@ def train(key):
 TRANSFORMATIONS = [
     upper_camel,
     upper_train,
-    upper_snake,
     upper_spaces,
+    upper_snake,
     snake,
     camel,
     train,
@@ -66,12 +66,12 @@ def get_file_list(directory):
     
     return [ f for f in file_list if f.endswith( ('.js', '.py', '.css') ) ]
 
-def get_rename_file_list(directory):
+def get_rename_file_list(directory, repo_root):
     file_list = []
     for root, _, files in os.walk(directory):
         if root.find("node_modules") == -1:
             for name in files:
-                file_list.append(os.path.join(root, name))
+                file_list.append(os.path.relpath(os.path.join(root, name), repo_root))
     
     return [ f for f in file_list if f.endswith( ('.js', '.py', '.css', '.png', '.svg') ) ]
 
@@ -92,11 +92,28 @@ def execute_reskin_on_file(file_path, config):
         for line in all_lines:
             reskin_file.write(modify_line(line, config))
 
+def execute_rename_on_file(file_path, config):
+    new_file_path = file_path
+    for old, new in reversed(sorted(config.items())):
+        for transformation in TRANSFORMATIONS:
+            if transformation(old) in new_file_path:
+                new_file_path = new_file_path.replace(transformation(old), transformation(new))
+    if new_file_path != file_path:
+        with open(new_file_path, "w") as new_file:
+            with open(file_path, "r") as old_file:
+                new_file.write(old_file.read())
+        os.remove(file_path)
+
 
 def execute_reskin(rename_dir, config):
     file_list = get_file_list(rename_dir)
     for file in file_list:
         execute_reskin_on_file(file, config)
+
+def execute_rename(rename_dir, config, repo_root):
+    rename_file_list = get_rename_file_list(rename_dir, repo_root)
+    for file_path in rename_file_list:
+        execute_rename_on_file(file_path, config)
 
 def main(skin_file):
     repo_root = os.path.dirname(os.path.abspath(__file__))
@@ -106,12 +123,8 @@ def main(skin_file):
         os.path.join(repo_root, "client"),
         os.path.join(repo_root, "server"),
     ]:
-        file_list = get_file_list(directory)
-        for file_path in file_list:
-            execute_reskin_on_file(file_path, config)
-        rename_file_list = get_rename_file_list(directory)
-        for file_path in rename_file_list:
-            pass
+        execute_reskin(directory, config)
+        execute_rename(directory, config, repo_root)
 
 
 if __name__ == "__main__":
